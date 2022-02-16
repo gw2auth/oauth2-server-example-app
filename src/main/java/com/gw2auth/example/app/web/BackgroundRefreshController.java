7a167d832163514e1343c51c3283368e32ac9093
Felix.9127 <felix@gw2auth.com>
2022-02-16 21:09:58 +0100

    tech: Revoke old tokens on new authorization with active refresh

--- src/main/java/com/gw2auth/example/app/web/BackgroundRefreshController.java
+++ src/main/java/com/gw2auth/example/app/web/BackgroundRefreshController.java
@@ -3,8 +3,10 @@ package com.gw2auth.example.app.web;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,12 +15,21 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.*;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.oauth2.core.OAuth2ErrorCodes;
+import org.springframework.security.oauth2.core.OAuth2RefreshToken;
+import org.springframework.security.oauth2.core.OAuth2Token;
+import org.springframework.security.oauth2.core.endpoint.OAuth2ParameterNames;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestOperations;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.util.*;
 
@@ -27,6 +38,7 @@ import java.util.*;
 public class BackgroundRefreshController {
 
     private static final Logger LOG = LoggerFactory.getLogger(BackgroundRefreshController.class);
+    private static final Duration CLOCK_SKEW = Duration.ofSeconds(5L);
 
     private final OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
     private final RefreshTokenOAuth2AuthorizedClientProvider refreshTokenOAuth2AuthorizedClientProvider;
@@ -34,6 +46,7 @@ public class BackgroundRefreshController {
     private final Object monitor;
     private final Set<String> addedPrincipals;
     private final Queue<OAuth2AuthorizedClient> clientsToBeRefreshed;
+    private final RestOperations revokeOldTokensRestTemplate;
 
     @Autowired
     public BackgroundRefreshController(OAuth2AuthorizedClientService oAuth2AuthorizedClientService, RefreshTokenOAuth2AuthorizedClientProvider refreshTokenOAuth2AuthorizedClientProvider) {
@@ -43,6 +56,7 @@ public class BackgroundRefreshController {
         this.monitor = new Object();
         this.addedPrincipals = new HashSet<>();
         this.clientsToBeRefreshed = new PriorityQueue<>(128, Comparator.comparing((v) -> v.getAccessToken().getExpiresAt()));
+        this.revokeOldTokensRestTemplate = new RestTemplateBuilder().build();
     }
 
     @GetMapping(value = "/api/background-refresh", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -107,7 +121,7 @@ public class BackgroundRefreshController {
             OAuth2AuthorizedClient next = pollNextToBeRefreshedClient();
 
             if (next != null) {
-                if (Instant.now().isAfter(next.getAccessToken().getExpiresAt())) {
+                if (Instant.now().isAfter(next.getAccessToken().getExpiresAt().minus(CLOCK_SKEW))) {
                     final String clientRegistrationId = next.getClientRegistration().getRegistrationId();
                     final String principalName = next.getPrincipalName();
                     final Authentication authentication = new NameAuthentication(principalName);
@@ -170,6 +184,8 @@ public class BackgroundRefreshController {
                 final OAuth2AuthorizedClient savedClient = this.oAuth2AuthorizedClientService.loadAuthorizedClient(clientRegistrationId, principalName);
 
                 if (savedClient != null && !next.getRefreshToken().getTokenValue().equals(savedClient.getRefreshToken().getTokenValue())) {
+                    revokeTokensSafe(next);
+
                     synchronized (this.monitor) {
                         this.clientsToBeRefreshed.offer(savedClient);
                     }
@@ -190,6 +206,48 @@ public class BackgroundRefreshController {
                         .principal(principal)
                         .build()
         );
+    }
+
+    private void revokeTokensSafe(OAuth2AuthorizedClient client) {
+        final ClientRegistration clientRegistration = client.getClientRegistration();
+
+        final MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+        params.add(OAuth2ParameterNames.CLIENT_ID, clientRegistration.getClientId());
+        params.add(OAuth2ParameterNames.CLIENT_SECRET, clientRegistration.getClientSecret());
+
+        final List<OAuth2Token> tokens = new ArrayList<>(2);
+        tokens.add(client.getAccessToken());
+
+        if (client.getRefreshToken() != null) {
+            tokens.add(client.getRefreshToken());
+        }
+
+        for (OAuth2Token token : tokens) {
+            if (token instanceof OAuth2AccessToken) {
+                params.set(OAuth2ParameterNames.TOKEN_TYPE_HINT, OAuth2ParameterNames.ACCESS_TOKEN);
+            } else if (token instanceof OAuth2RefreshToken) {
+                params.set(OAuth2ParameterNames.TOKEN_TYPE_HINT, OAuth2ParameterNames.REFRESH_TOKEN);
+            } else {
+                params.remove(OAuth2ParameterNames.TOKEN_TYPE_HINT);
+            }
+
+            params.set(OAuth2ParameterNames.TOKEN, token.getTokenValue());
+
+            final ResponseEntity<Void> responseEntity;
+            try {
+                responseEntity = this.revokeOldTokensRestTemplate.exchange(
+                        RequestEntity.post(clientRegistration.getProviderDetails().getIssuerUri() + "/oauth2/revoke")
+                                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                                .body(params),
+                        Void.class
+                );
+            } catch (Exception e) {
+                LOG.warn("failed to revoke token", e);
+                continue;
+            }
+
+            LOG.info("revoked token; got status={}", responseEntity.getStatusCode().value());
+        }
     }
 
     private static class NameAuthentication extends AbstractAuthenticationToken {
